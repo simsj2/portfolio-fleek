9e91a84c071905af30400c2a9494c2506af5f54f
Jared Sims <88754731+simsj2@users.noreply.github.com>
2022-10-10 12:49:04 -0500

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,9 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
 
-            <a href="" className="button">john@email.com</a>
-            <a href="https://twitter.com/" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/" className="button">LinkedIn</a>
-            <a href="https://github.com/" className="button">GitHub</a>
+            <a href="https://twitter.com/sims_j2" className="button">Twitter</a>
+            <a href="https://www.linkedin.com/in/jared-sims-bb4324240/" className="button">LinkedIn</a>
+            <a href="https://github.com/simsj2" className="button">GitHub</a>
         </div>
     );
 }
