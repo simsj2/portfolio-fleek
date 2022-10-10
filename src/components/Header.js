886f1aab72e56ff7748ea7494beef75822444a3c
Jared Sims <88754731+simsj2@users.noreply.github.com>
2022-10-10 12:48:30 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from '../assets/profile.PNG';
 
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src={profile} alt="Jared Sims" />
 
             <div className='header__content'>
-                <h1>Joe Blow</h1>
-                <p>Aspiring Blockchain Developer</p>
+                <h1>Jared Sims</h1>
+                <p>Blockchain Developer</p>
                 <a href="mailto:sims.j2siu@gmail.com" className='button'>Get In Touch</a>
             </div>
         </section>
